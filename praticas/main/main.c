ac42ca7c0dd871b7b9d4332203ed8a87c1c3051f
Henrique Costa <95293228+riquetret@users.noreply.github.com>
2025-09-03 11:16:09 -0300

    Pratica01
    Hello World e Print de Informacoes do ESP32

---
+++ praticas/main/main.c
@@ -0,0 +1,72 @@
+/*
+ * SPDX-FileCopyrightText: 2010-2022 Espressif Systems (Shanghai) CO LTD
+ *
+ * SPDX-License-Identifier: CC0-1.0
+ */
+
+#include <stdio.h>
+#include <inttypes.h>
+#include "sdkconfig.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_chip_info.h"
+#include "esp_flash.h"
+#include "esp_system.h"
+#include "esp_log.h"
+
+void app_main(void)
+{
+    static const char* TAG = "ESP_HENRIQUE";
+    ESP_LOGI(TAG,"Ola, mundo!"); // O esp-idf gerou esse comando sozinho
+
+    /* Print chip information */
+    esp_chip_info_t chip_info;  // Criacao do struct para informacoes
+    uint32_t flash_size;        // Variavel para tamanho do flash
+    esp_chip_info(&chip_info);  // Funcao que preenche o struct com as informacoes do chip
+
+    /*
+    CONFIG_IDF_TARGET = Modelo
+    chip_info.cores = Nª de nucleos
+    (chip_info.features & CHIP_FEATURE_WIFI_BGN) Verifica se constante wifi existe no struct e realiza operacao tenaria
+    Se sim sai wifi, se nao a string eh nula
+    (chip_info.features & CHIP_FEATURE_BT) Mesma coisa verificando se existe capacidades de bluetooth classic
+    (chip_info.features & CHIP_FEATURE_BLE) Mesma coisa verificando se existe capacide de bluetooth low energy
+    (chip_info.features & CHIP_FEATURE_IEEE802154) Mesma coisa verificando se possui suporte zigbee
+    */
+    ESP_LOGI(TAG,"Esse é o %s chip com %d CPU nucleo(s), %s%s%s%s, ",
+           CONFIG_IDF_TARGET,
+           chip_info.cores,
+           (chip_info.features & CHIP_FEATURE_WIFI_BGN) ? "WiFi/" : "",
+           (chip_info.features & CHIP_FEATURE_BT) ? "BT" : "",
+           (chip_info.features & CHIP_FEATURE_BLE) ? "BLE" : "",
+           (chip_info.features & CHIP_FEATURE_IEEE802154) ? ", 802.15.4 (Zigbee/Thread)" : "");
+
+    ESP_LOGI(TAG,"%d",chip_info.revision);  // chip_info.revision = 100
+    unsigned major_rev = chip_info.revision / 100;  // Revisao de Chip, provavelmente no formato XXX
+    unsigned minor_rev = chip_info.revision % 100;  // Revisao de Chip, chip_info.revision = 100, portanto zero
+    ESP_LOGI(TAG,"Versao do Esp v%d.%d, ", major_rev, minor_rev);
+
+    if(esp_flash_get_size(NULL, &flash_size) != ESP_OK) // Verifica se eh possivel obter
+    {
+        ESP_LOGE(TAG,"Nao foi possivel obter valor da flash");
+        return;
+    }
+
+    ESP_LOGE(TAG,"Nao foi possivel obter valor da flash");
+
+    ESP_LOGI(TAG,"%" PRIu32 "MB %s flash\n", flash_size / (uint32_t)(1024 * 1024),
+            (chip_info.features & CHIP_FEATURE_EMB_FLASH) ? "interna" : "externa");
+    ESP_LOGI(TAG,"Minimo espaco livre no Heap: %" PRIu32 " bytes\n", esp_get_minimum_free_heap_size());
+
+    for (int i = 2; i >= 0; i--) {
+        ESP_LOGI(TAG,"Testando vTask em %d seconds...\n",i);
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+    }
+
+    fflush(stdout);
+    while (1)
+    {
+        ESPLOGW(TAG,"Travou");
+    }
+    
+}
