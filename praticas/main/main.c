2e2d23acbb0f40c23fe69ef2ae3abd30742e101c
Henrique Costa <95293228+riquetret@users.noreply.github.com>
2025-09-15 13:59:25 -0300

    Comentarios, Ajuste na Pratica 1 e 2

--- praticas/main/main.c
+++ praticas/main/main.c
@@ -19,42 +19,87 @@
 #include "driver/gpio.h"
 
 ////////////////////////////////////////////////////
-#define GPIO_INPUT_IO_21    21
-#define GPIO_INPUT_IO_22    22
-#define GPIO_INPUT_IO_23    23
+#define GPIO_INPUT_IO_21    21  //Botao 0
+#define GPIO_INPUT_IO_22    22  //Botao 1
+#define GPIO_INPUT_IO_23    23  //Botao 2
+//1ULL é uma palavra de 64bits com o primeiro bit em 1, depois eh feito o deslocamento para configuracao correta
 #define GPIO_INPUT_PIN_SEL  ((1ULL<<GPIO_INPUT_IO_21) | (1ULL<<GPIO_INPUT_IO_22) | (1ULL<<GPIO_INPUT_IO_23)) 
 ////////////////////////////////////////////////////
-#define GPIO_OUTPUT_IO_2    2
+#define GPIO_OUTPUT_IO_2    2   //LED DA PLACA
 #define GPIO_OUTPUT_PIN_SEL  ((1ULL<<GPIO_OUTPUT_IO_2))
 ////////////////////////////////////////////////////
-#define ESP_INTR_FLAG_DEFAULT 0
+#define ESP_INTR_FLAG_DEFAULT 0 //Flag da interrupcao de IO
 static QueueHandle_t gpio_evt_queue = NULL;
+
 // Variáveis para debounce
-static int tickAnterior = 0;
-#define TEMPO_DEBOUNCE 250
+static unsigned int tickAnterior = 0;    //Tick da ultima mudanca do led quando pressiona Botao 2 (GPIO23)
+#define TEMPO_DEBOUNCE 250  //250ms de debounce
 
 static const char* TAG = "PRATICA1";
 static const char* TAG2 = "PRATICA2";
 
 static void IRAM_ATTR gpio_isr_handler(void* arg)
 {
-    uint32_t gpio_num = (uint32_t) arg;
+    /*  Gpio_num recebe o pino que sofreu interrupcao, 
+        o argumento eh o endereco "GPIO_INPUT_IO_XX"(21,22,23) 
+        que eh convertido para um inteiro */
+    uint32_t gpio_num = (uint32_t) arg; 
     xQueueSendFromISR(gpio_evt_queue, &gpio_num, NULL);
+}
+
+void configuraIO_pratica02(){
+    //ENTRADAS DOS BOTOES
+    gpio_config_t io_conf = {};
+    //interrupcao borda de descida
+    io_conf.intr_type = GPIO_INTR_NEGEDGE;
+    //set as output mode
+    io_conf.mode = GPIO_MODE_INPUT;
+    //bit mask of the pins that you want to set,e.g.GPIO18/19
+    io_conf.pin_bit_mask = GPIO_INPUT_PIN_SEL;
+    // Habilita Pull Up
+    io_conf.pull_up_en = 1;
+    //configure GPIO with the given settings
+    gpio_config(&io_conf);
+
+    //SAIDA LED, VOU REINICIAR POR SEGURANCA
+    //io_conf = {};
+    //disable interrupt
+    io_conf.intr_type = GPIO_INTR_DISABLE;
+    //set as output mode
+    io_conf.mode = GPIO_MODE_OUTPUT;
+    //bit mask of the pins that you want to set,e.g.GPIO18/19
+    io_conf.pin_bit_mask = GPIO_OUTPUT_PIN_SEL;
+    //disable pull-down mode
+    io_conf.pull_down_en = 0;
+    //disable pull-up mode
+    io_conf.pull_up_en = 0;
+    //configure GPIO with the given settings
+    gpio_config(&io_conf);
+    //create a queue to handle gpio event from isr
+    gpio_evt_queue = xQueueCreate(10, sizeof(uint32_t));
+    //install gpio isr service
+    gpio_install_isr_service(ESP_INTR_FLAG_DEFAULT);
+    //hook isr handler for specific gpio pin
+    gpio_isr_handler_add(GPIO_INPUT_IO_21, gpio_isr_handler, (void*) GPIO_INPUT_IO_21); //Atribue a interrupcao para funcao gpio_isr_handler
+    gpio_isr_handler_add(GPIO_INPUT_IO_22, gpio_isr_handler, (void*) GPIO_INPUT_IO_22);
+    gpio_isr_handler_add(GPIO_INPUT_IO_23, gpio_isr_handler, (void*) GPIO_INPUT_IO_23);
 }
 
 static void pratica02(void* arg)
 {
-    uint32_t io_num;
-    char acendeLed = 0;
-    int tickAtual;
+    configuraIO_pratica02();
+    char mensagens[][8] = {"Apagou","Acendeu","Cambiou"};   //Se GPIO22 mostra apaga, se GPIO21 mostra acende, se GPIO23 cambia led
+    char mensagen2[] = "LED";
+    uint32_t io_num;    // Qual GPIO foi acionado
+    char acendeLed = 0; // Variavel que representa o nivel logica do led
+    unsigned int tickAtual;  
+    // Converte milissegundos para ticks
+    unsigned short int debounce_ticks = pdMS_TO_TICKS(TEMPO_DEBOUNCE);
     for (;;) {          
-        if (xQueueReceive(gpio_evt_queue, &io_num, portMAX_DELAY)) {
-            tickAtual = xTaskGetTickCount();
+        if (xQueueReceive(gpio_evt_queue, &io_num, portMAX_DELAY)) {    //Coloca task bloqueada enquanto nao chegar evento GPIO
+            tickAtual = xTaskGetTickCount();    //Captura tempo atual
             if (io_num==23)
             {
-                // Converte milissegundos para ticks
-                int debounce_ticks = pdMS_TO_TICKS(TEMPO_DEBOUNCE);
-
                 // Se a diferença de tempo for menor que o tempo de debounce, ignora o evento
                 if (tickAtual - tickAnterior < debounce_ticks) {
                     continue;
@@ -62,14 +107,14 @@ static void pratica02(void* arg)
                 
                 // Atualiza o tempo do último clique
                 tickAnterior = tickAtual;
-                ESP_LOGI(TAG2,"Acendeu");
-                acendeLed = acendeLed ^ 1;
+                ESP_LOGI(TAG2,"%s %s",mensagens[2],mensagen2);
+                acendeLed = acendeLed ^ 1;  // Troca Nivel Logico
                 gpio_set_level(GPIO_OUTPUT_IO_2, acendeLed);
-            
             }
             else
             {
-                acendeLed = io_num%2;
+                acendeLed = io_num&1;   // Se GPIO21 io_num=21 (Botao 0) temos resultado 1, se GPIO22 (Botao 1) temos resultado 0
+                ESP_LOGI(TAG2,"%s %s",mensagens[acendeLed],mensagen2);
                 gpio_set_level(GPIO_OUTPUT_IO_2, acendeLed);
             }
         }
@@ -127,51 +172,13 @@ void app_main(void)
     fflush(stdout);
     
     ////////////////////////////////////////////////////
-    //ENTRADAS DOS BOTOES
-    gpio_config_t io_conf = {};
-    //interrupcao borda de descida
-    io_conf.intr_type = GPIO_INTR_NEGEDGE;
-    //set as output mode
-    io_conf.mode = GPIO_MODE_INPUT;
-    //bit mask of the pins that you want to set,e.g.GPIO18/19
-    io_conf.pin_bit_mask = GPIO_INPUT_PIN_SEL;
-    // Habilita Pull Up
-    io_conf.pull_up_en = 1;
-    //configure GPIO with the given settings
-    gpio_config(&io_conf);
 
-    //SAIDA LED, VOU REINICIAR POR SEGURANCA
-    //io_conf = {};
-    //disable interrupt
-    io_conf.intr_type = GPIO_INTR_DISABLE;
-    //set as output mode
-    io_conf.mode = GPIO_MODE_OUTPUT;
-    //bit mask of the pins that you want to set,e.g.GPIO18/19
-    io_conf.pin_bit_mask = GPIO_OUTPUT_PIN_SEL;
-    //disable pull-down mode
-    io_conf.pull_down_en = 0;
-    //disable pull-up mode
-    io_conf.pull_up_en = 0;
-    //configure GPIO with the given settings
-    gpio_config(&io_conf);
-
-    // CONFIGURA QUEU E TASK
-
-    //create a queue to handle gpio event from isr
-    gpio_evt_queue = xQueueCreate(10, sizeof(uint32_t));
-    //start gpio task
+    //Inicia task da pratica 2
     xTaskCreate(pratica02, "pratica02", 2048, NULL, 10, NULL);
-
-    //install gpio isr service
-    gpio_install_isr_service(ESP_INTR_FLAG_DEFAULT);
-    //hook isr handler for specific gpio pin
-    gpio_isr_handler_add(GPIO_INPUT_IO_21, gpio_isr_handler, (void*) GPIO_INPUT_IO_21);
-    gpio_isr_handler_add(GPIO_INPUT_IO_22, gpio_isr_handler, (void*) GPIO_INPUT_IO_22);
-    gpio_isr_handler_add(GPIO_INPUT_IO_23, gpio_isr_handler, (void*) GPIO_INPUT_IO_23);
 
     while (1)
     {
-        ESP_LOGW(TAG,"Travou");
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
+        ESP_LOGI(TAG,"Task 1 a cada 2 segundos");
+        vTaskDelay(2000 / portTICK_PERIOD_MS);
     }
 }
